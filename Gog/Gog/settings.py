8091bae8aae3d2e4f6522251d274d20d1e38262c
xiayun <xiay@baomihua.com>
2017-10-20 18:15:04 +0800

    update index.html

--- Gog/Gog/settings.py
+++ Gog/Gog/settings.py
@@ -82,14 +82,14 @@ WSGI_APPLICATION = 'Gog.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'devops_test',
-        'USER': 'devops',
-        'PASSWORD': 'devops',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       # 'ENGINE': 'django.db.backends.mysql',
+        #'NAME': 'devops_test',
+        #'USER': 'devops',
+        #'PASSWORD': 'devops',
+        #'HOST': '127.0.0.1',
+        #'PORT': '3306',
     }
 }
 
